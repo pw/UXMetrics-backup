37a75275440836e6c87f4e7aa835c2a7f2cd5d19
kaolti <kaolti@yahoo.com>
2019-10-04 20:55:46 +0100

    reverting webpack config, default should work

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,36 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const webpack = require('@rails/webpacker');
+const environment = require('./environment')
 
-// const environment = require('@rails/webpacker')
-const MinifyPlugin = require('babel-minify-webpack-plugin');
-
-const config = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  },
-  entry: './src/index.js',
-  output: {
-
-    filename: '[name].js',
-    path: __dirname + "/dist/js"
-  },
-  plugins: [
-   new MinifyPlugin()
- ]
-}
-
-module.exports = config;
+module.exports = environment.toWebpackConfig()
