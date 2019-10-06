6db16f0c87c8b99d26ba1661d496d447d408d1b6
kaolti <kaolti@yahoo.com>
2019-10-06 19:06:13 +0100

    added webpack config prod
    https://codereviewvideos.com/blog/how-i-fixed-uglifyjs-unexpected-token-name-dropin/

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,33 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = environment.toWebpackConfig()
+
+// module.exports = environment.toWebpackConfig()
+
+module.exports = function() {
+  console.log("BUILDING PRODUCTION");
+
+  return webpackMerge(commonConfig(), {
+
+    plugins: [
+
+      new UglifyJsPlugin({
+        test: /\.js($|\?)/i,
+        sourceMap: true,
+        uglifyOptions: {
+          mangle: {
+            keep_fnames: true,
+          },
+          compress: {
+            warnings: false,
+          },
+          output: {
+            beautify: false,
+          },
+        },
+      }),
+
+  });
+};
