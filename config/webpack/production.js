73ec2c1d0ab553eeadc5ff89d2a01e89b7faf9a5
kaolti <kaolti@yahoo.com>
2019-10-08 23:48:40 +0100

    config update

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,34 +3,16 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const webpack = require('@rails/webpacker');
 
 // const environment = require('@rails/webpacker')
-const MinifyPlugin = require('babel-minify-webpack-plugin');
-
-const config = {
+module.exports = {
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       }
     ]
-  },
-  entry: './src/index.js',
-  output: {
-
-    filename: '[name].js',
-    path: __dirname + "/dist/js"
-  },
-  plugins: [
-   new MinifyPlugin()
- ]
+  }
 }
-
-module.exports = config;
