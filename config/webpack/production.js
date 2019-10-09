9d15e9ed4876303a6b0cb30fb660fe8eb4220e27
kaolti <kaolti@yahoo.com>
2019-10-09 15:40:16 +0100

    fixed locally who knows what'll happen

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,18 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const webpack = require('@rails/webpacker');
+const environment = require('./environment')
 
-// const environment = require('@rails/webpacker')
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
-}
+module.exports = environment.toWebpackConfig()
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -200,3 +200,5 @@ function createTempColumn(){
 
 
 });
+
+import "controllers"
