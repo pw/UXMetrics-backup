efc09141f3d4fd178ec415e24e75d1b4c86536db
Patrick White <p@pwhite.org>
2020-09-22 01:28:44 -0600

    First commit as partner! Move Vue stuff into it's own javascript pack to stop messing with old card tests.

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -253,36 +253,3 @@ if ($("#homepage-signup")){
 });
 
 import "controllers"
-
-import TurbolinksAdapter from 'vue-turbolinks'
-import Vue from 'vue/dist/vue.esm'
-
-import VueNestable from 'vue-nestable'
-import VueClipboard from 'vue-clipboard2'
-
-import NewTreeTest from '../vue/tree_tests/new.vue'
-import EditTreeTest from '../vue/tree_tests/edit.vue'
-import TreeTestReport from '../vue/tree_tests/report.vue'
-
-import NewCardSort from '../vue/card_sorts/new.vue'
-import EditCardSort from '../vue/card_sorts/edit.vue'
-import CardSortReport from '../vue/card_sorts/report.vue'
-
-Vue.use(VueNestable)
-Vue.use(TurbolinksAdapter)
-Vue.use(VueClipboard)
-
-
-Vue.component('tree-test-new', NewTreeTest)
-Vue.component('tree-test-edit', EditTreeTest)
-Vue.component('tree-test-report', TreeTestReport)
-
-Vue.component('card-sort-new', NewCardSort)
-Vue.component('card-sort-edit', EditCardSort)
-Vue.component('card-sort-report', CardSortReport)
-
-document.addEventListener('turbolinks:load', function() { 
-  const app = new Vue({
-    el: '[data-behavior="vue"]',
-  })
-})
