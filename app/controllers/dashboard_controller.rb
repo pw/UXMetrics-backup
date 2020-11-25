00313bc51908b20602c187ffd50a811a5d00efe8
Patrick White <p@pwhite.org>
2020-11-24 17:58:25 -0700

    Card status counts on dashboard show.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,8 @@ class DashboardController < ApplicationController
 
   def show 
     cookies.delete :actioncable_user_id   
+    @card_sorts = current_user.card_sorts.group(:status).count
+    @tree_tests = current_user.tree_tests.group(:status).count
   end
 
   def verify
