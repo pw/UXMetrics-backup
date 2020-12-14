32512c3f6525642ea5cad8e0f57961e2bd493b13
Patrick White <p@pwhite.org>
2020-12-14 12:33:28 -0700

    Fix for subscriptions.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,4 +46,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.hosts << "uxmetrics.ngrok.io"
+  config.hosts << "uxmetrics2.ngrok.io"
 end
