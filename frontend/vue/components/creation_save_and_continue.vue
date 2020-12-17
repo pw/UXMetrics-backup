7eadd98f3a0d761bd3dccb21e4c1b1891c6cb4c1
uxops-dev <sean@uxops.com>
2020-12-16 16:39:51 -0800

    minor tweaks

--- frontend/vue/components/creation_save_and_continue.vue
+++ frontend/vue/components/creation_save_and_continue.vue
@@ -2,7 +2,7 @@
   <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
     <span class="inline-flex rounded-md shadow-sm">
       <button @click="$emit('click')" type="button" class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple active:bg-purple-700 transition duration-150 ease-in-out">
-        Save and Continue
+        Next
       </button>
     </span>
   </div>   
--- frontend/vue/tree_tests/new.vue
+++ frontend/vue/tree_tests/new.vue
@@ -74,7 +74,7 @@
         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
           <span class="inline-flex rounded-md shadow-sm">
             <button type="button" @click="next" class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple active:bg-purple-700 transition duration-150 ease-in-out">
-              Save and Continue
+              Next
             </button>
           </span>
         </div>  
@@ -100,7 +100,7 @@
         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
           <span class="inline-flex rounded-md shadow-sm">
             <button type="button" @click="next" class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple active:bg-purple-700 transition duration-150 ease-in-out">
-              Save and Continue
+              Next
             </button>
           </span>
         </div> 
@@ -131,7 +131,7 @@
          <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
            <span class="inline-flex rounded-md shadow-sm">
            <button type="button" @click="save" class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple active:bg-purple-700 transition duration-150 ease-in-out">
-               Save and Continue
+               Next
              </button>
            </span>
          </div> 
