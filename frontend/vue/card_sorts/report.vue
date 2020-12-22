abb76f2d09930df0c27b12acac7725f019ed3616
uxops-dev <sean@uxops.com>
2020-12-21 16:06:18 -0800

    fixing lock icon

--- frontend/vue/card_sorts/report.vue
+++ frontend/vue/card_sorts/report.vue
@@ -51,8 +51,8 @@
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="lg:flex lg:items-center lg:justify-between">
           <div class="flex justify-between">
-            <svg class="flex-initial mr-1 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />
+            <svg class="mr-1 inline h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
             </svg>
             <h5 class="flex-1 text-sm mb-6 lg:mb-0">
               This report is private. Only you and those with the password can view it. (<a href="'/' + test_type + '/' + id + '/edit'" class="text-purple-600 hover:text-purple-500 transition ease-in-out duration-150">Edit</a>)
