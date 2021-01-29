b01355d04c6b3806f9d27f899b1defc5fdbc8ed0
Patrick White <p@pwhite.org>
2021-01-28 22:36:58 -0700

    Updates to LogoUpload component.

--- frontend/vue/components/logo_upload.vue
+++ frontend/vue/components/logo_upload.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sm:col-span-4" :class="{'opacity-50': greyed_out}">
+  <div class="sm:col-span-4" :class="{'opacity-50': !enabled}">
     <label for="logo" class="block text-sm leading-5 font-medium text-gray-700">
         Logo
     </label>
@@ -7,7 +7,7 @@
     <img v-show="value !== ''" :src="logo_base_url + '/' + value" class="mb-6 w-40">
     <div class="mt-2 flex items-center">
       <span class="rounded-md shadow-sm">
-        <button @click="openUpload" type="button" :class="{'cursor-default': !enabled, 'hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800': !greyed_out}"" class="py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 transition duration-150 ease-in-out">
+        <button @click="openUpload" type="button" :class="{'cursor-default': !enabled, 'hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800': enabled}"" class="py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 transition duration-150 ease-in-out">
             Choose File
         </button>
       </span>
@@ -24,13 +24,13 @@ export default {
     value: String,
     instructions: String,
     logo_base_url: String,
-    enabled: {
+    allowed: {
       type: Boolean,
       default: true
     },
-    greyed_out: {
+    enabled: {
       type: Boolean, 
-      default: false
+      default: true
     }
   },
   data() {
@@ -39,23 +39,25 @@ export default {
   },
   methods: {
     openUpload() {
-      if(!this.enabled) {
-        this.$emit('attempt')
-        return
-      }
-      const options = {
-        fromSources: ['local_file_system', 'url'],
-        transformations: {
-          crop: true, 
-          rotate: true
-        },
-        accept: ['image/*'],
-        onUploadDone: (arg) => {
-          this.$emit('input', arg.filesUploaded[0].key)
+      if(this.allowed && this.enabled) {
+        const options = {
+          fromSources: ['local_file_system', 'url'],
+          transformations: {
+            crop: true, 
+            rotate: true
+          },
+          accept: ['image/*'],
+          onUploadDone: (arg) => {
+            this.$emit('input', arg.filesUploaded[0].key)
+          }
         }
-      }
-      filestack_client.picker(options).open()
-    }   
+        filestack_client.picker(options).open()
+      } else {
+        if(!this.allowed && this.enabled) {
+        this.$emit('attempt')
+        }
+      }   
+    }
   }
 }
 </script>
