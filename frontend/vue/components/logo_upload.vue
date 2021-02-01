97b8ed849848890ae9a2cd809bef83d467c581d6
Patrick White <p@pwhite.org>
2021-01-31 20:56:50 -0700

    Add way to remove uploaded logos

--- frontend/vue/components/logo_upload.vue
+++ frontend/vue/components/logo_upload.vue
@@ -7,9 +7,12 @@
     <img v-show="value !== ''" :src="logo_base_url + '/' + value" class="mb-6 w-40">
     <div class="mt-2 flex items-center">
       <span class="rounded-md shadow-sm">
-        <button @click="openUpload" type="button" :class="{'cursor-default': !enabled, 'hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800': enabled}"" class="py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 transition duration-150 ease-in-out">
+        <button v-if="value === ''" @click="openUpload" type="button" :class="{'cursor-default': !enabled, 'hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800': enabled}" class="py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 transition duration-150 ease-in-out">
             Choose File
         </button>
+        <button v-if="value !== ''" @click="$emit('input', '')" type="button" :class="{'cursor-default': !enabled, 'hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800': enabled}" class="py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 transition duration-150 ease-in-out">
+            Remove Logo
+        </button>        
       </span>
     </div>
   </div>  
