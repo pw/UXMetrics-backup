f35e461e49b17b5542fdf44cb9eebc1afa89d631
Patrick White <p@pwhite.org>
2021-02-02 23:03:55 -0700

    .

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
 
+if [ $PREVIEW_BUILD = 'true' ]
+then
+export FULL_APP_DOMAIN="https://${RAILS_HOST}.onrender.com"
+export FULL_APP_DOMAIN_WITHOUT_PROTOCOL="${RAILS_HOST}.onrender.com"
+export FULL_APP_DOMAIN_WITHOUT_PROTOCOL_OR_PORT="${RAILS_HOST}.onrender.com"
+fi
+
 bundle exec puma -C config/puma.rb
 
 
