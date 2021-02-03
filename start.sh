18884178dbd2120cd5bd5ef7798c455a6ab4516a
Patrick White <p@pwhite.org>
2021-02-02 23:15:07 -0700

    clean up from trying to get preview environments to work :-(

--- start.sh
+++ start.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-if [ $PREVIEW_BUILD = 'true' ]
-then
-export FULL_APP_DOMAIN="https://${RAILS_HOST}.onrender.com"
-export FULL_APP_DOMAIN_WITHOUT_PROTOCOL="${RAILS_HOST}.onrender.com"
-export FULL_APP_DOMAIN_WITHOUT_PROTOCOL_OR_PORT="${RAILS_HOST}.onrender.com"
-fi
-
 bundle exec puma -C config/puma.rb
 
 
