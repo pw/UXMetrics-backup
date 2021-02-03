09287f43bebc98010e7b989747e362cc6ac82c1e
Patrick White <p@pwhite.org>
2021-02-02 22:37:42 -0700

    .

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+
+if [ $PREVIEW_BUILD = 'false' ]
+then
 # exit on error
 set -o errexit
+fi
 
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean 
-if [ $PREVIEW_BUILD = 'true' ]
-then
-  bundle exec rails db:schema:load  
-fi
 bundle exec rails db:migrate
+
+
 
